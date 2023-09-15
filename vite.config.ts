6219b28d5c9208faf9bc9fe8e29dac82052ad8e3
naiaragabriela <naigabi31@gmail.com>
2023-09-14 22:09:39 -0300

    feat: convert video

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
